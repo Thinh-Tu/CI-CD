b36dc8ea2b074fddbf337465db51747c45cedee2
Thinh-Tu <124946393+Thinh-Tu@users.noreply.github.com>
2026-03-27 22:42:40 +0700

    new

--- app.js
+++ app.js
@@ -1,60 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-let mongoose = require('mongoose')
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-//localhost:3000/users
-app.use('/api/v1/users', require('./routes/users'));
-app.use('/api/v1/roles', require('./routes/roles'));
-app.use('/api/v1/products', require('./routes/products'))
-app.use('/api/v1/categories', require('./routes/categories'))
-app.use('/api/v1/inventories', require('./routes/inventories'))
-app.use('/api/v1/auth', require('./routes/auth'))
-
-
-mongoose.connect(process.env.MONGODB_URI);
-mongoose.connection.on('connected', function () {
-  console.log("connected");
-})
-mongoose.connection.on('disconnected', function () {
-  console.log("disconnected");
-})
-mongoose.connection.on('disconnecting', function () {
-  console.log("disconnecting");
-})
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+// Catch-all route to serve the interface
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(port, () => {
+    console.log(`🚀 Lightweight app listening heavily at http://localhost:${port}`);
 });
-
-module.exports = app;
